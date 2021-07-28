118daf7ac1f5c9e929c169367d7074c108163235
mikejigglypuff <50828241+mikejigglypuff@users.noreply.github.com>
2021-07-28 11:46:20 +0900

    Add files via upload

---
+++ number theory & combinatorics/1676.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace baekjoonC____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int result = 0;
+            for(int i = 1; i < n + 1; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    if (i % 25 == 0)
+                    {
+                        if (i % 125 == 0)
+                            result += 3;
+                        else
+                            result += 2;
+                    }
+                    else
+                        result += 1;
+                }    
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
