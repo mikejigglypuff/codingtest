84d20b0dcd0d101a698f59dfd11d5e2cc6cb8a62
mikejigglypuff <50828241+mikejigglypuff@users.noreply.github.com>
2021-08-23 15:36:52 +0900

    Add files via upload

---
+++ dynamic programming 1/2579.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace baekjoonC____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> scores = new List<int>();
+            for (int i = 0; i < n; i++)
+                scores.Add(int.Parse(Console.ReadLine()));
+            List<int> dp = new List<int>();
+
+            dp.Add(scores[0]);
+            if(n > 1)
+                dp.Add(scores[0] + scores[1]);
+            if(n > 2)
+                dp.Add(Math.Max(scores[0], scores[1]) + scores[2]);
+            for (int i = 3; i < n; i++)
+                dp.Add(Math.Max(dp[i - 3] + scores[i - 1], dp[i - 2]) + scores[i]);
+            
+
+            Console.WriteLine(dp[n - 1]);
+        }
+    }
+}
