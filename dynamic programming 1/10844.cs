e26859d33882950a3e4bf4afe6b688e1e5742850
mikejigglypuff <50828241+mikejigglypuff@users.noreply.github.com>
2021-08-25 13:08:58 +0900

    Add files via upload

---
+++ dynamic programming 1/10844.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace baekjoonC____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[,] dp = new long[n, 10];
+
+            dp[0, 0] = 0;
+            for (int i = 1; i <= 9; i++)
+                dp[0, i] = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                dp[i, 0] = dp[i - 1, 1] % 1000000000;
+                for (int j = 1; j < 9; j++)
+                    dp[i, j] = (dp[i - 1, j - 1] + dp[i - 1, j + 1]) % 1000000000;
+                dp[i, 9] = dp[i - 1, 8] % 1000000000;
+            }
+
+            long sum = 0;
+            for (int i = 0; i <= 9; i++)
+                sum += dp[n - 1, i];
+            
+
+            Console.WriteLine(sum % 1000000000);
+        }
+    }
+}
