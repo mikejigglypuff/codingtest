d6e4db7c8c18d4a78734e55ca6b43593762c07b7
mikejigglypuff <k24555569@gmail.com>
2024-03-11 23:48:37 +0900

    class4 시작

---
+++ class4/b1043.java
@@ -0,0 +1,16 @@
+package class4;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class b1043 {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String[] knows_real = br.readLine().split(" ");
+
+        } catch (IOException e) {}
+    }
+}
